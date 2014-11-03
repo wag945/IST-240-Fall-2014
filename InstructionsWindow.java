b937f7687bc84a83fe65b381c47bef3e633b8e9a
Ron <ravila327@gmail.com>
2014-11-02 19:55:05 -0500

    added Jeopardy Instruction Image, what do you guys think? -Ron

--- InstructionsWindow.java
+++ InstructionsWindow.java
@@ -9,20 +9,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class InstructionsWindow extends JPanel implements ActionListener {
+public class InstructionsWindow extends JPanel implements ActionListener 
+
+{
     
-    JTextField text;
+//    JTextField text;
+//    JButton instructionWindow;
+    
     public InstructionsWindow()
     {
         super();
         setBackground(Color.BLUE);
-        text = new JTextField();
-        text.setText("Instructions go here...");
-        add(text);
-        setVisible(false);
+//        text = new JTextField();
+//        text.setText("Instructions go here...\n");
+//        text.setText("test test test\n");
+//        text.setText("test test1, test2, test3 \n");
+//        add(text);
+//        setVisible(false);
+        
+//        instructionWindow = new JButton();
     }
 
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+
+            ImageIcon instructionImage = new ImageIcon("src/images/JeopardyInstructions.jpg");
+            Image gameInstructionImage = instructionImage.getImage();
+            g.drawImage(gameInstructionImage, 50, 10, this);
+        }
+        
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) 
+    {
+
     }
 }

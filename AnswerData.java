0c469a1793e2fa02aa770ede3447fcf840c4fa4b
williamganley <william.ganley@missionse.com>
2014-11-08 13:01:18 -0500

    Add xml parsing

--- AnswerData.java
+++ AnswerData.java
@@ -1,28 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
+ * @Course: IST 240 (FA 14)
+ * @Section: 001
  *
- * @author Bill
+ * @Group 08
  */
 public class AnswerData {
-    
+
     int questionId;
     String category;
     String question;
-    String answer1,answer2,answer3;
+    String answer1, answer2, answer3;
     int correctAnswer;
-    
-    public AnswerData(  int id,
-                        String theCategory,
-                        String theQuestion,
-                        String theAnswer1,
-                        String theAnswer2,
-                        String theAnswer3,
-                        int theCorrectAnswer)
-    {
+
+    public AnswerData(int id,
+            String theCategory,
+            String theQuestion,
+            String theAnswer1,
+            String theAnswer2,
+            String theAnswer3,
+            int theCorrectAnswer) {
         questionId = id;
         category = theCategory;
         question = theQuestion;
@@ -31,73 +27,60 @@ public class AnswerData {
         answer3 = theAnswer3;
         correctAnswer = theCorrectAnswer;
     }
-    
-    int getQuestionId()
-    {
+
+    int getQuestionId() {
         return questionId;
     }
-    
-    void setQuestionId(int id)
-    {
+
+    void setQuestionId(int id) {
         questionId = id;
     }
-    
-    String getCategory()
-    {
+
+    String getCategory() {
         return category;
     }
-    
-    void setCategory(String theCategory)
-    {
+
+    void setCategory(String theCategory) {
         category = theCategory;
     }
-    String getQuestion()
-    {
+
+    String getQuestion() {
         return question;
     }
-    
-    void setQuestion(String theQuestion)
-    {
+
+    void setQuestion(String theQuestion) {
         question = theQuestion;
     }
-    
-    String getAnswer1()
-    {
+
+    String getAnswer1() {
         return answer1;
     }
-    
-    void setAnswer1(String theAnswer)
-    {
+
+    void setAnswer1(String theAnswer) {
         answer1 = theAnswer;
     }
-    
-    String getAnswer2()
-    {
+
+    String getAnswer2() {
         return answer2;
     }
-    
-    void setAnswer2(String theAnswer)
-    {
+
+    void setAnswer2(String theAnswer) {
         answer2 = theAnswer;
     }
-    
-    String getAnswer3()
-    {
+
+    String getAnswer3() {
         return answer3;
     }
-    
-    void setAnswer3(String theAnswer)
-    {
+
+    void setAnswer3(String theAnswer) {
         answer3 = theAnswer;
     }
-    
-    int getCorrectAnswer()
-    {
+
+    int getCorrectAnswer() {
         return correctAnswer;
     }
-    
-    void setCorrectAnswer(int theCorrectAnswer)
-    {
+
+    void setCorrectAnswer(int theCorrectAnswer) {
         correctAnswer = theCorrectAnswer;
     }
 }

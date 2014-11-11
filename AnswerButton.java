d7804121a40f2adb44437a3b454d2aab2e2c739c
williamganley <william.ganley@missionse.com>
2014-11-10 20:27:01 -0500

    Connect AnswerButton to Answer

--- AnswerButton.java
+++ AnswerButton.java
@@ -10,20 +10,42 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 class AnswerButton extends JButton {
-    String question,answer;
     QuestionFrame questionFrame;
     Boolean IsAnswerCorrect;
+    myJFrame parentFrame;
+    Answer answer;
+    int answerDataId;
+    AnswerData answerData;
+    int difficulty;
+    String difficultyStr;
 
-    AnswerButton(String text,String inputQuestion,String inputAnswer) 
+    AnswerButton(String label,myJFrame frame, Answer inputAnswer, int id, int Difficulty) 
     {
         super();
         setOpaque(true);
         setBackground(Color.DARK_GRAY);
         setFont(new Font("Dialog", 1, 20));
         this.setForeground(new Color(0, 205, 0, 255));
-        setText(text);
-        question = inputQuestion;
+        setText(label);
         answer = inputAnswer;
+        answerDataId = id;
+        difficulty = Difficulty;
+        switch(difficulty)
+        {
+            case 1:
+                difficultyStr = "K12";
+                break;
+            case 2:
+                difficultyStr = "Undergrad";
+                break;
+            case 3:
+                difficultyStr = "Grad";
+                break;
+        }
+        System.out.println("Answer difficultyStr = "+difficultyStr);
+        answerData = answer.getAnswerDataByQuestionId(difficultyStr, answerDataId);
+        System.out.println("AnswerButton ");
+        answer.printAnswerData(answerData);
     } 
     
     public void onButtonPressed()
@@ -36,7 +58,7 @@ class AnswerButton extends JButton {
     
     public void displayQuestion()
     {
-        questionFrame = new QuestionFrame();
+        questionFrame = new QuestionFrame(answerData);
         questionFrame.displayWindow();
     }
     
